cecae8240a441651fab015753ce878757f1cee78
Honey-bee-code <hanifinida@gmail.com>
2021-05-10 22:51:19 +0800

    17 express contact app - listening message

--- 17-express-contact-app/app.js
+++ 17-express-contact-app/app.js
@@ -126,5 +126,5 @@ app.use("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Contact app listening at http://localhost:${port}`);
 });
