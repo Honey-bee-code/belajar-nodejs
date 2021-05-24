3d3c434ccc525015f1c354fe18787f1ed91436b0
Honey-bee-code <hanifinida@gmail.com>
2021-05-24 23:12:32 +0800

    finishing CRUD

--- 17-express-contact-app/app.js
+++ 17-express-contact-app/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
-const { loadContacts, findContact, addContact, cekDuplikat } = require("./utilities/contacts");
+const { loadContacts, findContact, addContact, cekDuplikat, deleteContact, updateContacts } = require("./utilities/contacts");
 const { body, check, validationResult } = require("express-validator");
 const session = require("express-session");
 const cookieParser = require("cookie-parser");
@@ -22,109 +22,168 @@ app.use(express.urlencoded({ extended: true })); // untuk paring req.body
 // konfigurasi flash
 app.use(cookieParser("secret"));
 app.use(
-  session({
-    cookie: { maxAge: 6000 },
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
-  })
+    session({
+        cookie: { maxAge: 6000 },
+        secret: "secret",
+        resave: true,
+        saveUninitialized: true,
+    })
 );
 app.use(flash());
 
 app.get("/", (req, res) => {
-  res.render("index", { layout: "layouts/main-layout", title: "Homepage" });
+    res.render("index", { layout: "layouts/main-layout", title: "Homepage" });
 });
 
 app.get("/about", (req, res, next) => {
-  res.render("about", { layout: "layouts/main-layout", title: "About Page" });
+    res.render("about", { layout: "layouts/main-layout", title: "About Page" });
 });
 
 app.get("/contact", (req, res) => {
-  const contacts = loadContacts();
-  res.render("contact", {
-    layout: "layouts/main-layout",
-    title: "Contact Page",
-    contacts,
-    msg: req.flash("msg"),
-  });
+    const contacts = loadContacts();
+    res.render("contact", {
+        layout: "layouts/main-layout",
+        title: "Contact Page",
+        contacts,
+        msg: req.flash("msg"),
+    });
 });
 
 app.get("/contact/add", (req, res) => {
-  const id = "";
-  const nohp = "";
-  const email = "";
-  const nama = "";
+    const id = "";
+    const nohp = "";
+    const email = "";
+    const nama = "";
 
-  res.render("contact-add", {
-    layout: "layouts/main-layout",
-    title: "Add Contact",
-    id,
-    nohp,
-    email,
-    nama,
-  });
-});
-
-app.post(
-  "/contact",
-
-  // validator
-  check("id", "<i class='bi bi-person-badge'></i>   ID harus terisi !").notEmpty(),
-  body("id").custom((value) => {
-    const duplikat = cekDuplikat(value);
-    if (duplikat) {
-      throw new Error("<i class='bi bi-person-badge'></i>    ID sudah terpakai, silahkan gunakan ID yang lain !");
-    }
-    return true;
-  }),
-  check("nama", "<i class='bi bi-person-check-fill'></i>   Nama harus terisi !").notEmpty(),
-  check("nohp", "<i class='bi bi-telephone-plus'></i>   No HP harus terisi !").notEmpty(),
-  check("nohp", "<i class='bi bi-telephone-plus'></i>   Format No HP salah !").isMobilePhone("id-ID"),
-  check("email", "<i class='bi bi-envelope'></i>    Format Email salah !").isEmail(),
-
-  (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      // res.send(errors.array());
-
-      const id = req.body.id;
-      const nohp = req.body.nohp;
-      const email = req.body.email;
-      const nama = req.body.nama;
-      res.render("contact-add", {
+    res.render("contact-add", {
         layout: "layouts/main-layout",
         title: "Add Contact",
-        errors: errors.array(),
         id,
         nohp,
         email,
         nama,
-      });
-      return false;
-    }
+    });
+});
 
-    addContact(req.body);
-    // kirimkan flash message
-    req.flash("msg", "Contact was successfully added !");
-    res.redirect("/contact");
-  }
+app.post(
+    "/contact",
+
+    // validator
+    check("id", "<i class='bi bi-person-badge'></i>   ID harus terisi !").notEmpty(),
+    body("id").custom((value) => {
+        const duplikat = cekDuplikat(value);
+        if (duplikat) {
+            throw new Error("<i class='bi bi-person-badge'></i>    ID sudah terpakai, silahkan gunakan ID yang lain !");
+        }
+        return true;
+    }),
+    check("nama", "<i class='bi bi-person-check-fill'></i>   Nama harus terisi !").notEmpty(),
+    check("nohp", "<i class='bi bi-telephone-plus'></i>   No HP harus terisi !").notEmpty(),
+    check("nohp", "<i class='bi bi-telephone-plus'></i>   Format No HP salah !").isMobilePhone("id-ID"),
+    check("email", "<i class='bi bi-envelope'></i>    Format Email salah !").isEmail(),
+
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // res.send(errors.array());
+
+            const id = req.body.id;
+            const nohp = req.body.nohp;
+            const email = req.body.email;
+            const nama = req.body.nama;
+            res.render("contact-add", {
+                layout: "layouts/main-layout",
+                title: "Add Contact",
+                errors: errors.array(),
+                id,
+                nohp,
+                email,
+                nama,
+            });
+        } else {
+            addContact(req.body);
+            // kirimkan flash message
+            req.flash("msg", "Contact was successfully added !");
+            res.redirect("/contact");
+        }
+    }
+);
+app.get("/contact/delete/:id", (req, res) => {
+    const id = req.params.id;
+    const contact = findContact(id);
+    if (!contact) {
+        res.send("<h1>404: page not found!</h1>");
+    } else {
+        deleteContact(id);
+        req.flash("msg", "Contact was successfully deleted !");
+        res.redirect("/contact");
+    }
+});
+
+app.get("/contact/edit/:id", (req, res) => {
+    const id = req.params.id;
+    const contact = findContact(id);
+
+    res.render("contact-edit", {
+        layout: "layouts/main-layout",
+        title: "Add Contact",
+        contact,
+    });
+});
+
+app.post(
+    "/contact/update",
+
+    // validator
+    check("id", "<i class='bi bi-person-badge'></i>   ID harus terisi !").notEmpty(),
+    body("id").custom((value, {req}) => {
+        const duplikat = cekDuplikat(value);
+        if (value !== req.body.oldId && duplikat) {
+            throw new Error("<i class='bi bi-person-badge'></i>    ID sudah terpakai, silahkan gunakan ID yang lain !");
+        }
+        return true;
+    }),
+    check("nama", "<i class='bi bi-person-check-fill'></i>   Nama harus terisi !").notEmpty(),
+    check("nohp", "<i class='bi bi-telephone-plus'></i>   No HP harus terisi !").notEmpty(),
+    check("nohp", "<i class='bi bi-telephone-plus'></i>   Format No HP salah !").isMobilePhone("id-ID"),
+    check("email", "<i class='bi bi-envelope'></i>    Format Email salah !").isEmail(),
+
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            // res.send(errors.array());
+
+            res.render("contact-edit", {
+                layout: "layouts/main-layout",
+                title: "Edit Contact",
+                errors: errors.array(),
+                contact: req.body,
+            });
+        } else {
+            updateContacts(req.body);
+            // kirimkan flash message
+            req.flash("msg", "Contact was successfully updated !");
+            res.redirect("/contact");
+
+        }
+    }
 );
 
 app.get("/contact/:id", (req, res) => {
-  const id = req.params.id;
-  const contact = findContact(id);
-  res.render("detail", {
-    layout: "layouts/main-layout",
-    title: "Contact Detail",
-    contact,
-  });
+    const id = req.params.id;
+    const contact = findContact(id);
+    res.render("detail", {
+        layout: "layouts/main-layout",
+        title: "Contact Detail",
+        contact,
+    });
 });
 
 app.use("/", (req, res) => {
-  res.status(404);
-  res.send("<h1>404: page not found!</h1>");
+    res.status(404);
+    res.send("<h1>404: page not found!</h1>");
 });
 
 app.listen(port, () => {
-  console.log(`Contact app listening at http://localhost:${port}`);
+    console.log(`Contact app listening at http://localhost:${port}`);
 });
--- 17-express-contact-app/utilities/contacts.js
+++ 17-express-contact-app/utilities/contacts.js
@@ -4,46 +4,62 @@ const fs = require("fs");
 // membuat folder data jika belum ada
 const dirPath = "./data";
 if (!fs.existsSync(dirPath)) {
-  fs.mkdirSync(dirPath);
+    fs.mkdirSync(dirPath);
 }
 
 // membuat file contacts.json jika belum ada
 const contactsPath = "./data/contacts.json";
 if (!fs.existsSync(contactsPath)) {
-  fs.writeFileSync(contactsPath, "[]", "utf-8");
+    fs.writeFileSync(contactsPath, "[]", "utf-8");
 }
 
 // membaca isi contacts.json
 const loadContacts = () => {
-  const fileBuffer = fs.readFileSync("data/contacts.json", "utf-8");
-  const contacts = JSON.parse(fileBuffer);
-  return contacts;
+    const fileBuffer = fs.readFileSync("data/contacts.json", "utf-8");
+    const contacts = JSON.parse(fileBuffer);
+    return contacts;
 };
 
 // mencari contact berdasarkan id
 const findContact = (id) => {
-  const contacts = loadContacts();
-  const contact = contacts.find((contact) => contact.id === id);
-  return contact;
+    const contacts = loadContacts();
+    const contact = contacts.find((contact) => contact.id === id);
+    return contact;
 };
 
 // menuliskan / menimpa file contacts.json dengan data baru
 const saveContacts = (contacts) => {
-  fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
+    fs.writeFileSync("data/contacts.json", JSON.stringify(contacts));
 };
 
 // menambah data contact baru
 const addContact = (contact) => {
-  const contacts = loadContacts();
-  contacts.push(contact);
-  saveContacts(contacts);
+    const contacts = loadContacts();
+    contacts.push(contact);
+    saveContacts(contacts);
 };
 
 // cek duplikat id
 const cekDuplikat = (id) => {
-  const contacts = loadContacts();
-  const contact = contacts.find((contact) => contact.id === id);
-  return contact;
+    const contacts = loadContacts();
+    const contact = contacts.find((contact) => contact.id === id);
+    return contact;
 };
 
-module.exports = { loadContacts, findContact, addContact, cekDuplikat };
+// menghapus data contact
+const deleteContact = (id) => {
+    const contacts = loadContacts();
+    const filteredContacts = contacts.filter((contact) => contact.id !== id);
+    saveContacts(filteredContacts);
+};
+
+// mengubah data contact
+const updateContacts = (contactBaru) => {
+    const contacts = loadContacts();
+    const filteredContacts = contacts.filter((contact) => contact.id !== contactBaru.oldId);
+    // saveContacts(filteredContacts, contactBaru);
+    delete contactBaru.oldId;
+    filteredContacts.push(contactBaru);
+    saveContacts(filteredContacts);
+};
+module.exports = { loadContacts, findContact, addContact, cekDuplikat, deleteContact, updateContacts };
